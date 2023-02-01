5f9d2400e35adf7f0b3449356c8226c61daeb70a
Leyla-Akkurt <leylaacinar@gmail.com>
2023-02-01 17:01:16 +0300

    form02 is added

---
+++ form02/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { InteractiveWelcome } from "./InteractiveWelcome";
+
+
+export class App extends React.Component{
+ 
+
+render(){
+  return (
+   <InteractiveWelcome/>
+  )
+}
+}
+

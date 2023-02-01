576039d4731e894f17c0894e76360fdb9e3296e6
Leyla-Akkurt <leylaacinar@gmail.com>
2023-02-01 16:22:42 +0300

    form01 folder is added

---
+++ form01/src/InteractiveWelcome.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Welcome } from "./Welcome";
+
+export class InteractiveWelcome extends React.Component{
+    state={
+        username:""
+    }
+    handleOnChangeInput= (event)=>{
+        
+        this.setState(
+            {username:event.target.value}
+            )
+    }
+
+    render(){
+        return (
+            <div>
+                 <input name="username" onChange={this.handleOnChangeInput}/>
+                 <Welcome name={this.state.username}/>
+                
+            </div>
+        )
+    }
+}
+

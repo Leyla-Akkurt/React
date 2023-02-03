856624923151bd96c37e0e7c1284dd5dc1392f27
Leyla-Akkurt <leylaacinar@gmail.com>
2023-02-03 14:43:04 +0300

    list03 is added

---
+++ form06/src/UncontrolledLogin.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+export class UncontrolledLogin extends React.Component{
+    
+
+     componentDidMount(){
+        this.nameInput.focus(); 
+     } 
+
+     
+
+    state={
+        username:"",
+        password:"",
+       
+    }
+   
+    handleFormSubmit=(event)=>{
+        const username=event.elements.username.value;
+        const password=event.elements.password.value;
+        console.log({
+            username,
+            password
+        })
+    }
+
+    render(){
+        return(
+            <div>
+            <div>
+                <h1>Uncontrolled Login</h1>
+            </div>
+            
+            <div>
+                <form onSubmit={this.handleFormSubmit}>
+                        <input
+                        ref={(input) => { this.nameInput = input; }} 
+                        name="username" 
+                        />
+
+                        <input
+                        name="password"
+                        type="password"
+                        />
+
+                        <button
+                        name="login"
+                        type="submit"
+                        >Login</button>
+                    
+                        <button
+                        name="reset"
+                        type="reset"
+                        >Reset</button>
+                </form>
+            </div>
+            </div>
+        )
+    }
+}

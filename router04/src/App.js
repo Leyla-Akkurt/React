eb179673a83a9523d8b47b05acd35b629ca0f48f
Leyla-Akkurt <leylaacinar@gmail.com>
2023-02-10 21:46:40 +0300

    router04 is done

---
+++ router04/src/App.js
@@ -0,0 +1,24 @@
+
+import { ClickCounter } from "./ClickCounter";
+import { Link, Route, Routes } from "react-router-dom";
+import { Welcome } from "./Welcome";
+import { ShowGithubUser } from "./ShowGithubUser";
+
+
+export  function App({name,username}){
+
+      return (
+        <div>
+          <Link to="/"> Welcome </Link> ||  <Link to="/counter"> Counter</Link> || <Link to="/:username"> UserName</Link>
+      <Routes>
+        <Route path="/" element={<Welcome name={name}/>}/>
+        <Route path="/counter" element={<ClickCounter />}/>
+        <Route path="/:username" element={<ShowGithubUser username={username}/>}/>
+        
+      </Routes>
+      </div>
+      
+      )
+  }
+
+
---
+++ router04/src/ShowGithubUser.js
@@ -0,0 +1,8 @@
+import { GithubUser } from "./GithubUser";
+
+export function ShowGithubUser({username}){
+    
+    return (
+        <GithubUser username={username}/>
+    )
+}
